9d1357dfdc4ffeb470cbfdf1781bb9ab3aed64f8
piitomice123 <piitomice-123@hotmail.co.th>
2018-01-17 15:08:58 +0700

    Init project

---
+++ src/main/java/Rmuti/lab3/StackandQueue/TestQueue.java
@@ -0,0 +1,15 @@
+package Rmuti.lab3.StackandQueue;
+
+public class TestQueue {
+
+	public static void main(String[] args) {
+		ArrayListQueue queue = new ArrayListQueue();
+		queue.enqueue("macos");
+		queue.enqueue("linux");
+		queue.enqueue("windows");
+		System.out.println(queue);
+		String data = (String)queue.dequeue();
+		System.out.println(queue);
+	}
+
+}
